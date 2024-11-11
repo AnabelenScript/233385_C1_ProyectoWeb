42333d507e40e8f81da37baf98d65e9a5df6ac4c
AnabelenScript <ana0507belen@gmail.com>
2024-11-11 01:03:57 -0600

    adding appweb to amazon

--- 233385_C1_ProyectoWeb/dist/233385-c1-proyecto-web/server/main.server.mjs
+++ 233385_C1_ProyectoWeb/dist/233385-c1-proyecto-web/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FAK5GQLF.mjs";import"./chunk-5YNICMZV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-KVHE5YMD.mjs";import"./chunk-NSIC6M5Z.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- 233385_C1_ProyectoWeb/src/resources/imagecard/shoeService.ts
+++ 233385_C1_ProyectoWeb/src/resources/imagecard/shoeService.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ShoeService {
-  private readonly apiUrl = 'http://localhost:3000/zapatos';
+  private readonly apiUrl = 'https://54.156.54.129/zapatos';
 
   constructor(private http: HttpClient) {}
 
